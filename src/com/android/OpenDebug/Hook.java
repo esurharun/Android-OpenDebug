8d12f76952129d9e9a54fc12bc5fc67a424ecb88
Harun Esur <harun.esur@arneca.com>
2014-12-10 07:19:39 +0200

    Fixed for Android 4.4 problems

--- src/com/android/OpenDebug/Hook.java
+++ src/com/android/OpenDebug/Hook.java
@@ -9,13 +9,23 @@ import com.saurik.substrate.*;
 public class Hook {
 	private static String _TAG = "OpenDebug";
 	   static void initialize() {
+		   
+		   /*
+		    * start(final String processClass,
+389                                   final String niceName,
+390                                   int uid, int gid, int[] gids,
+391                                   int debugFlags, int mountExternal,
+392                                   int targetSdkVersion,
+393                                   String seInfo,
+394                                   String[] zygoteArgs) {
+		    */
 
             MS.hookClassLoad("android.os.Process", new MS.ClassLoadHook() {
                 public void classLoaded(Class<?> _class) {
-                    hookMethod(_class, "start", 
-                    		new Class<?>[] {String.class, String.class, Integer.TYPE, 
-                    			Integer.TYPE, int[].class, Integer.TYPE, Integer.TYPE,
-                            	Integer.TYPE, String.class, String[].class});
+                    hookMethod(_class, "startViaZygote", 
+                    		new Class<?>[] {String.class, String.class, int.class, 
+                    			int.class, int[].class, int.class, int.class,
+                            	int.class, String.class, String[].class});
                 }
             });
 	   }
@@ -30,8 +40,14 @@ public class Hook {
             Log.i(_TAG, "NoSuchMethod: " + methodName + 
             		" with " + params.length + " args");
         }
+        
+        
+        
  
         if (method != null) {
+        
+        	
+//        	final Method pMethod = method;
         	Log.i(_TAG, "Hooking " + methodName + 
         			"() with " + params.length + " args");
             MS.hookMethod(_class, method,
@@ -39,6 +55,16 @@ public class Hook {
                 public Object invoked(final Context hooked,
                 		final Object... args) throws Throwable {
                 			args[5] = ((Integer) args[5] | 0x1);
+                			
+//                			Object[] modArgs = new Object[] {
+//                					args[0],args[0],args[1],args[2],args[3],args[4],args[5],args[6],args[7]
+//                			};
+                		
+//                			Log.i(_TAG, "Method: "+pMethod.toGenericString());
+//                			for (int i=0;i<args.length;i++) {
+//                				Log.i(_TAG,"Args ("+i+")"+" "+args[i].getClass().getName()+" : "+args[i]);
+//                			}
+                			
                 			return invoke(hooked, args);
                 }
         	});
